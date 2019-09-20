ccefe6ae5e7c11ad183380539b90f31060e3321c
chungmcl <chungmcl@hotmail.com>
2019-09-19 23:40:21 -0700

    revert to pre-concurrency project

--- src/com/chungmcl/Main.java
+++ src/com/chungmcl/Main.java
@@ -1,23 +1,16 @@
 package com.chungmcl;
 
-import java.lang.Math;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class Main {
+
     // The size of the chess board. (Default chess boards are 8 x 8)
-    private static final int boardSize = 8;
+    static final int boardSize = 8;
 
     // Possible "moves" a knight can make represented by deltaX/deltaY
-    private static final int[][] possibleMoves = new int[][] {
-        {2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -1}, {2, -1}
+    static final int[][] possibleMoves = new int[][] {
+            {2, 1}, {2, -1}, {-2, 1}, {-2, -1 }, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}
     };
-    
+
+
     public static void main(String[] args)
     {
         // A 2D array of numbers to represent a chessboard
@@ -30,26 +23,26 @@ public class Main {
         board[0][0] = 0;
 
         // Returns false if a knight's tour could not be generated
-        if (!RunKnightsTour(board, 1, 0, 0))
+        if (!KnightsTour(board, 1, 0, 0))
         {
             System.out.println("A knights tour could not be generated.");
         }
     }
 
     // Initialize all board squares to -1 (-1 representing not having touched the square)
-    private static void InitializeBoard(int[][] board)
+    public static void InitializeBoard(int[][] board)
     {
-        for (int x = 0; x < boardSize; x++)
+        for (int a = 0; a < boardSize; a++)
         {
-            for (int y = 0; y < boardSize; y++)
+            for (int b = 0; b < boardSize; b++)
             {
-                board[x][y] = -1;
+                board[a][b] = -1;
             }
         }
     }
 
     // Recursive function to generate a knights tour
-    private static boolean RunKnightsTour(int[][] board, int moveCount, int currentX, int currentY)
+    public static boolean KnightsTour(int[][] board, int moveCount, int currentX, int currentY)
     {
         //System.out.println(moveCount);
         if (moveCount == Math.pow(boardSize, 2)) // If Knight touches all squares on chessboard
@@ -67,36 +60,19 @@ public class Main {
         }
         else // If Knight hasn't touched all squares yet
         {
-            ExecutorService executor = Executors.newFixedThreadPool(possibleMoves.length);
-            List<Future<Boolean>> results = new ArrayList<Future<Boolean>>();
             for (int i = 0; i < possibleMoves.length; i++)
             {
                 int newX = currentX + possibleMoves[i][0];
                 int newY = currentY + possibleMoves[i][1];
                 if (ValidateMove(newX, newY, board))
                 {
-                    final int[][] boardCopy = CopyBoard(board);
-                    boardCopy[newX][newY] = moveCount;
-                    
-                    results.add(executor.submit(
-                        new Callable<Boolean>() {
-                            public Boolean call() {
-                                return RunKnightsTour(board, moveCount, currentX, currentY);
-                            }
-                        }
-                    ));
-                }
-            }
-            // Join the boi
-            for (Future<Boolean> x : results)
-            {
-                try 
-                {
-                    if (x.get())
+                    //int[][] boardCopy = CopyBoard(board);
+                    board[newX][newY] = moveCount;
+
+                    // Recursively call KnightsTour
+                    if (KnightsTour(board, moveCount + 1, newX, newY))
                         return true;
-                } catch (Exception e) {
-                    System.out.println("EXCEPTION THROWN: " + e);
-                    return false;
+                    board[newX][newY] = -1;
                 }
             }
             return false;
@@ -105,7 +81,7 @@ public class Main {
 
     // Check if the move is valid
     // (Doesn't leave the board, doesn't go to a square that the Knight has already been to)
-    private static boolean ValidateMove(int newX, int newY, int[][] board)
+    public static boolean ValidateMove(int newX, int newY, int[][] board)
     {
         return (
                 (newX >= 0 && newX < boardSize)
@@ -115,7 +91,7 @@ public class Main {
     }
 
     // Return a reference to a new copy of a board
-    private static int[][] CopyBoard(int[][] board)
+    public static int[][] CopyBoard(int[][] board)
     {
         int[][] newBoard = new int[boardSize][boardSize];
         for (int i = 0; i < boardSize; i++)
